a04bf86c91a897a2e7b3f283b3c38021829b3fba
Gabriel Oliveira Aragão <31046857+linknasubi@users.noreply.github.com>
2020-03-27 12:09:41 -0300

    Update Graphic.py

--- Graphic.py
+++ Graphic.py
@@ -1,172 +1,235 @@
-from graphics import *
+import pyautogui
 import time
-from random import randint
-
-WIDTH, HEIGHT = 900, 800
-
-
-
+import numpy as np
+import PIL.ImageGrab as ImageGrab
+import numpy as np
+pyautogui.FAILSAFE = True
 
 
-#        
-class General():
+def sigmoid(x):
+    return (1/(1+np.exp(-x)))
+
+
+def initial_Values():
     
-    def __init__(self, number_dinos):
-        self.dino_dic = {}
-        self.number_dinos = number_dinos
-        self.speed = 1
-        self.distance = 2.01
-        
+    vision, w_o, w_h = [], [], []
     
-    def dinosCreation(self):
+    
+    for i in range(5):
+    
+        vision.append([np.random.randint(0, len(im_matrix), size = spots_number), 
+                               np.random.randint(0, len(im_matrix[0]), size = spots_number)])
         
-        for i in range (self.number_dinos):
+        w_h.append([np.random.rand(len(feature_set), nodes_num)])
+    
+        w_o.append([np.random.rand(len(hidden_neurons), 1)])
+    
+    return vision, w_h, w_o
+
+
+def mutated_Values(vis, wei_h, wei_o):
+    vision, w_o, w_h = [[], []], [], []
+    gen_vision = []
+
+    
+    for i in range (5):
+        mut_rate = (np.random.randint(-5,5)/100)
+    
+        vision[0] = (np.multiply(vis[0], mut_rate))
+        vision[1] = (np.multiply(vis[1], mut_rate))
+        vision[0] = [int(x) for x in vision[0]]
+        vision[1] = [int(x) for x in vision[1]]
+        gen_vision.append([(vision[0]),(vision[1])])
+#        vision[0] = vision[0].astype(np.int64)
+#        vision[1] = vision[1].astype(np.int64)
+        
+        w_h.append(np.multiply(wei_h, mut_rate))
+    
+        w_o.append(np.multiply(wei_o, mut_rate))
+    
+    return gen_vision, w_h, w_o
+
+
+
+over = False
+
+time.sleep(2)
+
+start_time = 0
+res_config = [1920, 1080]
+
+np.random.seed(7)
+
+box_area_visions = {}
+number_of_visions = 6
+spots_number = 6
+
+
+#for i in range (number_of_visions):
+#    box_area_vision = (((i+1)*(res_config[0]//2))//(number_of_visions+1), res_config[1]//4, 
+#                       ((i+2)*(res_config[0]//2)-10)//(number_of_visions+1), res_config[1]//2)
+#    im = ImageGrab.grab(box_area_vision)
+#    box_area_visions[i] = im
+
+
+
+
+box_area_vision = (((1)*(res_config[0]//2))//(number_of_visions+1), res_config[1]//4, 
+                       ((2)*(res_config[0]//2)-10)//(number_of_visions+1), res_config[1]//2)
+
+
+
+game_over_vision = (4*res_config[0]//20, 6*res_config[1]//20, 
+                    6*res_config[0]//20, 8*res_config[1]//20)
+img_over = ImageGrab.grab(game_over_vision)
+arr_over = np.array(img_over)
+
+
+
+
+im = ImageGrab.grab(box_area_vision)
+im_matrix = np.array(im)
+
+
+ 
+
+selec_pixels = [np.random.randint(0, len(im_matrix), size = spots_number), 
+                np.random.randint(0, len(im_matrix[0]), size = spots_number)]
+
+
+
+nodes_num = 6
+
+feature_set = np.array(selec_pixels[0])
+hidden_weights = np.random.rand(len(feature_set), nodes_num)
+hidden_weights_1 = np.random.rand(nodes_num, nodes_num)
+hidden_neurons = np.zeros([nodes_num])
+hidden_neurons_1 = np.zeros([nodes_num])
+output_weights = np.random.rand(len(hidden_neurons), 1)
+
+
+vision_values, weights_h, weights_o = initial_Values()
+time_values = []
+
+
+
+
+def jump():
+    pyautogui.keyDown('space')
+    time.sleep(0.05)
+    pyautogui.keyUp('space')
+
+
+def appending_values(flag, vision_values, weight_h, weight_o):
+    vision = []
+    weight_1 = []
+    weight_2 = []
+    time = []
+    vision.append(vision_values)
+    weight_1.append(weight_h)
+    weight_2.append(weight_o)
+    time.append(time.time())
+
+counter, idx_counter, counter_flag = 0, -1, 0
+time_flag = True
+
+while True:
+    
+    over = False
+    
+    if arr_over[23][100][0] == 83 and arr_over[69][100][0] == 83:
+        
+        
+        print(counter)
+        
+        game_over_vision = (4*res_config[0]//20, 6*res_config[1]//20, 
+                            6*res_config[0]//20, 8*res_config[1]//20)
+        img_over = ImageGrab.grab(game_over_vision)
+        arr_over = np.array(img_over)
+        
+        
+        
+        over=True
+        time_flag = True        
+
+        
+        if counter == 5:
+            print('passou')
+            idx_counter = -1
+
+            best_idx = time_values.index(max(time_values))
             
-            dino_position = randint(50,100)
-            dino_location_x = (WIDTH//2)+dino_position
-            dino_location_y = HEIGHT//2
-            self.dino = Rectangle(Point(dino_location_x , dino_location_y), Point(dino_location_x+50, dino_location_y+50))
-            self.dino.setFill(color_rgb(randint(0,255), randint(0,255), randint(0,255)))
+            counter = 0
             
-            self.dino_dic[i] = [self.dino, dino_location_x, dino_location_y]
-    
-    def cactusCreation(self):
-        
-        self.cactus = Rectangle(Point(WIDTH-50, (HEIGHT//2)+50), Point(WIDTH, ((HEIGHT//2)-randint(1, 200))))
-        self.cactus.setFill('green')
-        
-        return self.cactus
-
-    def worldGen(self):
-        
-        self.dinosCreation()
-        self.cactusCreation()
-        
-        self.win = GraphWin('Dino Evolution', WIDTH, HEIGHT)
-        
-        self.floor = Rectangle(Point(0, (HEIGHT//2)+51), Point(WIDTH, (HEIGHT//2)+60))
-        self.floor.setFill('brown')
-        
-        self.floor.draw(self.win)
-        self.cactus.draw(self.win)
-        
-        for i in range(self.number_dinos):    
-            self.dino_dic[i][0].draw(self.win)
-        
-        time.sleep(0.3)
-#       
-    
-    def moving(self):
-        
-        self.worldGen() 
-        while self.speed <= 30:
-            self.distance += self.speed*0.02
-            print(self.distance)
-            time.sleep(0.02)
-            self.speed += 0.005
-            self.cactus.move(-self.speed, 0)
+            vision_values, weights_h, weights_o = mutated_Values(vision_values[best_idx],
+                                                                 weights_h[best_idx],
+                                                                 weights_o[best_idx])
+            del time_values[:]
             
-            if int(self.distance)%22 == 1:
-
-                self.cactus.undraw()
-                self.cactus = self.cactusCreation()
-                self.cactus.draw(self.win)
-            
-            if any(self.dino_dic[1]) ==
-        
-        while self.speed > 30:
-            time.sleep(0.02)
-            print(self.distance)
-            self.distance += self.speed*0.02
-            self.cactus.move(-self.speed, 0)  
-            if int(self.distance)%22 == 1:
-                
-                self.cactus.undraw()
-                self.cactus = self.cactusCreation()
-                self.cactus.draw(self.win)
-    
-#        
-
-
-#            self.distance += self.speed
-#            self.gen_controller(self.speed)
-#            time.sleep(0.05)
-#            self.speed += 0.01
-#            self.player.move(self.speed, self.gravity)
-#            if self.jump == True:
-#                self.player.move(self.speed, self.jump_force + self.gravity)
-    
+              
+        if counter_flag == 0:
+            time.sleep(0.5)
+            print('a')
+            jump()
+            counter += 1
+            idx_counter += 1
+            counter_flag = 1
         
 
-#din = Dino(4)
-a = General(15)
-#a.worldGen()
-a.moving()
+        
+    if time_flag == True:
+        start_time = time.time()
+        actual_time = time.time() - start_time
+        time_values.append(actual_time)
+        
+        time_flag = False
+    
+    
+    
+    
+    
+    if over == False:
+        counter_flag = 0
+   
+        time.sleep(0.05)
+        
+        
+        game_over_vision = (4*res_config[0]//20, 6*res_config[1]//20, 
+                             6*res_config[0]//20, 8*res_config[1]//20)
+        img_over = ImageGrab.grab(game_over_vision)
+        arr_over = np.array(img_over)
+        
+        
+       
+        
+        box_area_vision = (((1)*(res_config[0]//2))//(number_of_visions+1), res_config[1]//4, 
+                           ((2)*(res_config[0]//2)-10)//(number_of_visions+1), res_config[1]//2)
+    
+        im = ImageGrab.grab(box_area_vision)
+        im_matrix = np.array(im)
+        feature_set = []
+        
+        
+        for i, j in zip(vision_values[idx_counter][0], vision_values[idx_counter][1]):
+            feature_set.append(im_matrix[i][j][0])
+    
+        print(feature_set)
+    
+        
+        
+        output_neurons = np.zeros([1])     
+        hidden_neurons = sigmoid(np.dot(feature_set, hidden_weights[idx_counter]))
+        hidden_neurons_1 = sigmoid(np.dot(hidden_neurons, hidden_weights_1[idx_counter]))
+        output_neurons = sigmoid(np.dot(output_weights.T, hidden_neurons_1))
+        
+    
+        
 
-#getattr(Dino, 'dino_size')
-
-
-
-
-
-#for i in range (number_of_dinos):
-#    
-#    dino = Rectangle(Point(50, HEIGHT//2), Point(100, (HEIGHT//2)+50))
-#    dino.setFill(color_rgb(randint(0,255), randint(0,255), randint(0,255)))
-#    
-#    self.dinos_dic[i] = dino
-
-
-
-#
-#class Dinos(object):
-#    
-#    def __init__(self):
-#        
-#
-#        self.gravity = 2
-#        self.jump_force = -10
-#        self.speed = 1
-#        self.distance = 0
-#        
-#        self.win = GraphWin('Lab Four', WIDTH, HEIGHT)
-#    
-#        self.player = Rectangle(Point(50, HEIGHT//2), Point(100, (HEIGHT//2)+50))
-#        self.player.setFill('blue')
-#        self.floor = Rectangle(Point(0, (HEIGHT//2)+51), Point(WIDTH, (HEIGHT//2)+60))
-#        self.floor.setFill('brown')
-#
-#    
-#        self.player.draw(self.win)
-#        self.floor.draw(self.win)
-#        
-#        
-#    
-#    def gen_controller(self, distance):
-#        self.jump = False
-#        if (distance%60) == 1:
-#            self.cactus = Rectangle(Point(WIDTH-50, HEIGHT//2), Point(WIDTH, (HEIGHT//2)+randint(1, 200)))
-#            self.cactus.setFill('green')
-#            self.cactus.draw(self.win)
-#
-#    def moving_controller(self):
-#        self.gen_controller()
-#        
-#        
-#
-#    def moving(self):
-#        
-#        while self.distance < (WIDTH//2) - 50:
-#
-#            self.distance += self.speed
-#            self.gen_controller(self.speed)
-#            time.sleep(0.05)
-#            self.speed += 0.01
-#            self.player.move(self.speed, self.gravity)
-#            if self.jump == True:
-#                self.player.move(self.speed, self.jump_force + self.gravity)
+        print(output_neurons)
+        
+        if output_neurons > 0.8:
+            jump()
 
 
-#
-#Din = Dinos()
-#Din.moving()
+
+
